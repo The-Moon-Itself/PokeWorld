084654868bda3f7706bb607843139f95e1e6295e
The-Moon-Itself <51838176+The-Moon-Itself@users.noreply.github.com>
2025-08-05 17:39:22 -0400

    More resprites and trader fix

---
+++ Compat/BS_Sapient_Animals/1.6/Source/SapientAnimalsPatches/SapientAnimalsPatches/SapientAnimals_PawnRenderNode_HAnimalPart_GraphicFor_Patch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapientAnimalsPatches
+{
+    internal class SapientAnimals_PawnRenderNode_HAnimalPart_GraphicFor_Patch
+    {
+    }
+}
